68261bbf1f055c564d3bdf2442ec9ae2cdbb1651
Seynabou12 <dioneseynabou0@gmail.com>
2021-09-03 17:16:53 +0000

    ajout de fichier

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,21 +2,22 @@
 
 namespace App\Controller\Admin;
 
-use App\Repository\RestaurantRepository;
+
+use App\Entity\Users;
+use App\Entity\Restaurant;
 use App\Repository\UsersRepository;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Repository\RestaurantRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Restaurant;
-use App\Entity\Users;
-use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\User\UserInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
 
 class DashboardController extends AbstractDashboardController
 {
-    
     protected $usersRespository;
     protected $restaurantRepository;
 
@@ -26,7 +27,6 @@ class DashboardController extends AbstractDashboardController
         $this->restaurantRepository = $restaurantRepository;
 
     }
-
     /**
      * @Route("/admin", name="admin")
      */
@@ -52,7 +52,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Restaurants', 'fas fa-hamburger', Restaurant::class);
         yield MenuItem::linkToCrud('Utilisateurs', 'fa fa-user', Users::class);
     }
-
     //syteme pour gerer les gravatar
     
     public function configureUserMenu(UserInterface $user): UserMenu
@@ -60,7 +59,6 @@ class DashboardController extends AbstractDashboardController
         return parent::configureUserMenu($user)
         ->setName($user->getUserIdentifier())
         ->setGravatarEmail($user->getUserIdentifier())
-       // ->setAvatarUrl('') pour donner des liens par exemple le github;
         ->displayUserAvatar('true');
     }
 }
